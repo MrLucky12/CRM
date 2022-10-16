86abc418618481c52bfb456a79a1136df6691431
MrLucky12 <leomore2000@hotmail.com>
2022-10-15 23:21:08 -0500

    admin template

--- CRM/src/app/app.module.ts
+++ CRM/src/app/app.module.ts
@@ -4,12 +4,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
-import { routing } from './app.routing'
+import { routing } from './app.routing';
+import { SidebarComponent } from './components/sidebar/sidebar.component';
+import { FooterComponent } from './components/footer/footer.component'
 
 @NgModule({
   declarations: [
     AppComponent,
-    DashboardComponent
+    DashboardComponent,
+    SidebarComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
