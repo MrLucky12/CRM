0735aa610660ac4e6d26ce04d7abd49886d913be
MrLucky12 <leomore2000@hotmail.com>
2022-10-16 16:08:04 -0500

    Modelo de colaborador

---
+++ backend/models/Colaborador.js
@@ -0,0 +1,19 @@
+
+var mongoose = require('mongoose')
+var Schema = mongoose.Schema;
+
+var ColaboradorSchema = Schema({
+    name: {type: String, required:true},
+    lastName: {type: String, required:true},
+    email: {type: String, required:true},
+    fullname: {type: String, required:true},
+    phone: {type: String, required:false},
+    role: {type: String, required:true},
+    gender: {type: String, required:true},
+    n_doc: {type: String, required: false},
+    password: {type: String, required:true},
+    state: {type: Boolean, default: true, required:true},
+    country: {type: String, required:false}
+})
+
+module.exports = mongoose.model('colaborador', ColaboradorSchema);
