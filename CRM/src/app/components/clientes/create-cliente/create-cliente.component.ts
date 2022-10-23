06d543de51dbfcaa768b2a469aa7df5e8dc620c5
MrLucky12 <leomore2000@hotmail.com>
2022-10-23 13:45:36 -0500

    Validacion de campos independientes

--- CRM/src/app/components/clientes/create-cliente/create-cliente.component.ts
+++ CRM/src/app/components/clientes/create-cliente/create-cliente.component.ts
@@ -25,8 +25,54 @@ export class CreateClienteComponent implements OnInit {
   }
 
   registrar(registroForm: any) {
+    console.log(registroForm);
     // VALIDACION DE FORMULARI | CORREO EXISTENTE | ETC?
-    if (registroForm.valid) { 
+    if (!registroForm.value.name) {
+      $.notify('El campo nombre esta incompleto', { 
+        type: 'danger',
+        spacing: 10,                    
+        timer: 2000,
+        placement: { from: 'top',  align: 'right' },
+        delay: 1000,
+        animate: { enter: 'animated ' + 'bounce', exit: 'animated ' + 'bounce' }
+        });
+    } else if (!registroForm.value.lastName) {
+      $.notify('El campo apellido esta incompleto', { 
+        type: 'danger',
+        spacing: 10,                    
+        timer: 2000,
+        placement: { from: 'top',  align: 'right' },
+        delay: 1000,
+        animate: { enter: 'animated ' + 'bounce', exit: 'animated ' + 'bounce' }
+        });
+    } else if (!registroForm.value.email) {
+      $.notify('El campo email esta incompleto', { 
+        type: 'danger',
+        spacing: 10,                    
+        timer: 2000,
+        placement: { from: 'top',  align: 'right' },
+        delay: 1000,
+        animate: { enter: 'animated ' + 'bounce', exit: 'animated ' + 'bounce' }
+        });
+    } else if (!registroForm.value.gender) {
+      $.notify('El campo genero esta incompleto', { 
+        type: 'danger',
+        spacing: 10,                    
+        timer: 2000,
+        placement: { from: 'top',  align: 'right' },
+        delay: 1000,
+        animate: { enter: 'animated ' + 'bounce', exit: 'animated ' + 'bounce' }
+        });
+    } else if (!registroForm.value.phone) {
+      $.notify('El campo telefono esta incompleto', { 
+        type: 'danger',
+        spacing: 10,                    
+        timer: 2000,
+        placement: { from: 'top',  align: 'right' },
+        delay: 1000,
+        animate: { enter: 'animated ' + 'bounce', exit: 'animated ' + 'bounce' }
+        });
+    } else {
       this.btn_registrar = true;
       this._clienteService.registro_cliente_admin(this.cliente, this.token).subscribe(
           response=>{ 
@@ -42,7 +88,7 @@ export class CreateClienteComponent implements OnInit {
                 this.btn_registrar = false;
             } else {  
               this.btn_registrar = false;
-              $.notify('Colaborador agregado exitosamente !', { 
+              $.notify('Cliente agregado exitosamente !', { 
                 type: 'success',
                 spacing: 10,                    
                 timer: 2000,
@@ -54,17 +100,6 @@ export class CreateClienteComponent implements OnInit {
             }
           }); 
     }
-    else{
-      // this.btn_registrar = true;
-      $.notify('Complete correctamente el formulario', { 
-        type: 'danger',
-        spacing: 10,                    
-        timer: 2000,
-        placement: { from: 'top',  align: 'right' },
-        delay: 1000,
-        animate: { enter: 'animated ' + 'bounce', exit: 'animated ' + 'bounce' }
-        }); 
-      }
   }
 
 
