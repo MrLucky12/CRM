965b2d4d78c1abd7a37bb6d73844b069b0bf835e
MrLucky12 <leomore2000@hotmail.com>
2022-10-23 17:48:56 -0500

    envio de correo de verificacion de cuenta v1.0

--- backend/controllers/clienteController.js
+++ backend/controllers/clienteController.js
@@ -2,6 +2,14 @@
 var Cliente = require('../models/Cliente');
 var bcrypt = require('bcrypt-nodejs');
 
+// MAIL SENDER | CONFIRMATION
+var fs = require('fs');
+var handlebars = require('handlebars');
+var ejs = require('ejs');
+var nodemailer = require('nodemailer');
+var smtpTransport = require('nodemailer-smtp-transport');
+var path = require('path');
+
 const registro_cliente_admin = async function(req, res) {
     // console.log(req.user);
     if ( req.user ) {
@@ -21,6 +29,7 @@ const registro_cliente_admin = async function(req, res) {
                         data.fullname = data.name +' '+ data.lastName;
                         data.password = hash;
                         let cliente = await Cliente.create(data);
+                        enviar_correo_verificacion(cliente.email);
                         res.status(200).send({data: cliente});    
                     }
                 }
@@ -33,6 +42,10 @@ const registro_cliente_admin = async function(req, res) {
 
     } 
     else{ res.status(403).send({data: undefined, message: 'No Token'}); }
+}
+
+const enviar_correo_verificacion = async function(email) {
+
 }
 
 module.exports = {
